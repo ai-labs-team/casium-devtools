18dc62483d6369822e1e9b8627c8ab61c0bb6822
Peter Chapman <peterchappy@users.noreply.github.com>
2017-10-25 17:28:46 -0400

    working

--- test-generator.js
+++ test-generator.js
@@ -1,5 +1,8 @@
-import { keys, } from 'ramda';
+import { keys, lensPath, view, set, traverse } from 'ramda';
 import hjson from 'hjson';
+import { diff } from 'json-diff';
+import { isModifiedObject, isModifiedArray } from './util';
+
 
 const formatOpts = {
   condense: 80,
@@ -19,14 +22,19 @@ export const generateUnitTest = (msg) => {
     .map((str, i) => i === 0 ? str : '  ' + str)
     .join('\n');
 
-  var prevState = toJsVal(msg.prev);
+  const delegateLens = lensPath(msg.path);
+  var prevState = toJsVal(view(delegateLens, msg.prev));
   var msgData = keys(msg.data).length ? toJsVal(msg.data) : '';
   var newState = toJsVal(msg.next);
+  var relay = toJsVal(msg.relay);
   var hasCommands = msg.commands && msg.commands.length;
   var runPrefix = hasCommands ? `const commands = ` : '';
 
+  console.log(traverse(diff(view(delegateLens, msg.prev), msg.next)));
+
   var lines = [`it('should respond to ${msg.message} messages', () => {`];
-  lines.push(`  container.push(${prevState});`);
+  lines.push(`  isolate(${msg.name}, { relay : ${relay} })`);
+  lines.push(`  container.push(${hasCommands ? prevState : prevState});`);
   lines.push(`  ${runPrefix}container.dispatch(new ${msg.message}(${msgData}));`);
   lines.push('');
   lines.push(`  expect(container.state()).to.deep.equal(${newState});`);
