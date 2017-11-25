d7e02b49fd295738e08f7d4dfdd22157085dc20c
Nate Abele <nate.abele@gmail.com>
2017-11-26 01:23:45 +0200

    Rename

--- devtools.js
+++ devtools.js
@@ -3,4 +3,4 @@
 // chrome.extension.*
 
 // Create a tab in the devtools area
-chrome.devtools.panels.create("Architecture", null, "panel.html", function(panel) {});
+chrome.devtools.panels.create("Casium", null, "panel.html", function(panel) {});
