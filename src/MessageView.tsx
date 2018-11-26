7a98d3970dfa86ea68118cd3a046053de21c8276
Warren Seymour <warren@fountainhead.tech>
2018-11-26 12:52:20 +0000

    Eliminate React warning about missing `key` prop in MessageView

--- src/MessageView.tsx
+++ src/MessageView.tsx
@@ -93,8 +93,8 @@ export class MessageView extends React.Component<Props, State> {
       const data = (useDependencyTrace && dependencyTrace) ? deepPick(msg.data || {}, dependencyTrace.message) : msg.data || {};
 
       const relayItem = Object.keys(relay).length > 0 ? [
-        <div className="panel-label">Relay</div>,
-        <ObjectInspector data={relay} expandLevel={0} mapper={nodeMapper} />
+        <div className="panel-label" key="relay">Relay</div>,
+        <ObjectInspector data={relay} expandLevel={0} mapper={nodeMapper} key="relay-inspector" />
       ] : null;
 
       return (
